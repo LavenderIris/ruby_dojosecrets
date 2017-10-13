75036d5e8422a881aebf5f779c87f91e19e2bbcb
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-13 14:51:37 -0700

    likes model done

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,8 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_uniqueness_of :email
   
-
-
   def email= email
     super(email.try(:downcase))
   end
-
-  
   
 end
