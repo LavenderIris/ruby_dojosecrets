c2ea22d8cd1feeb6ee86821a1d79c65b1f5ad949
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-13 14:44:59 -0700

    added secrets table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
   has_secure_password
+
   validates :name, presence: true
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_uniqueness_of :email
   
+
+
   def email= email
     super(email.try(:downcase))
   end
+
+  
+  
 end
