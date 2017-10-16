2f415596b7e3093f5e33a3854a54a357e09e9ad3
Priscilla Finder <priscilla@Priscillas-MacBook-Pro.local>
2017-10-16 13:08:49 -0700

    like features

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def create
+    Like.create(like_params)
+
+    redirect_to "/secrets"
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    if current_user === @like.user
+      @like.destroy 
+    end
+    redirect_to "/secrets/"
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:secret_id, :user_id)
+  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :secret
+  validates_uniqueness_of :secret 
+
+
 end
